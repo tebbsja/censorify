bd5250a8f1d49c2855dd95c9b6c80a2dc1f0354b
tebbsja <stinden1@yahoo.com>
2015-10-20 21:02:53 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var fs = require('fs');
 var http = require('http');
 var url = require('url');
-var ROOT_DIR = "/Users/Austin/Desktop/weather/node.js/";
+var ROOT_DIR = "html/";
 
 http.createServer(function(req, res) {
   var urlObj = url.parse(req.url, true, false);
